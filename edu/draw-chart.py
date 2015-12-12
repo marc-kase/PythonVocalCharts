666f68f798405b05af0ae4c3d97e03c64fcfd8a3
marcgereski <marc.gereski@gmail.com>
2015-12-12 21:00:19 +0600

    new data

--- edu/draw-chart.py
+++ edu/draw-chart.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy
 
 source = 'source.csv'
+data = 'source.csv'
 data0 = 'data0.csv'
 data1 = 'data.csv'
 data2 = 'data2.csv'
@@ -23,9 +24,16 @@ with open(source) as my_file:
 #     for row in r:
 #         y = row
 
-t = numpy.arange(0., 32768, 4.)
+print(len(y))
+freq = 16384.
+
+t = freq / (len(y))
+if t == 0: t = 1
+
+x = numpy.arange(0., freq, t)
+# print(len(x))
 
 # plt.plot(y)
-plt.plot(t, y)
+plt.plot(x, y)
 plt.show()
 plt.savefig(target)
